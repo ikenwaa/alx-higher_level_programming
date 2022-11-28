489cf59381cd49b569c9cfc6abaa4297e953960f
ikenwaa <ajohn9353@gmail.com>
2022-11-28 09:12:26 -0800

    Added 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if a linked list contains a cycle
+ * @list: linked list to check from
+ * Return: 1 if cycle is present, 0 if absent
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (list == NULL)
+		return (0);
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
