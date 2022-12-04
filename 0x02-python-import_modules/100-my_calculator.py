fac2799dca16a38993ae9f8910a0650755a066e5
ikenwaa <ajohn9353@gmail.com>
2022-12-04 05:03:25 -0800

    Added 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    """Performs basic arithmetic operations"""
     import sys
     from calculator_1 import add, sub, mul, div
 
     if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>", end="")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     operators = {"+": add, "-": sub, "*": mul, "/": div}
