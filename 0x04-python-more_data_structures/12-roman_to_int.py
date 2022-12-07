d181b17e7fe2543e75c9b9895823551475755ecc
ikenwaa <ajohn9353@gmail.com>
2022-12-07 15:49:36 -0800

    Added 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) is not str or roman_string == "":
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+    if roman_string == "":
         return 0
     num = 0
     roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
