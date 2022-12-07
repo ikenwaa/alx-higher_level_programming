adb61734b434001a7faca295e9093ac218786ac4
ikenwaa <ajohn9353@gmail.com>
2022-12-07 15:16:17 -0800

    Added 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
-    keys = list(new_dict.key())
+    keys = list(new_dict.keys())
 
     for key in keys:
         new_dict[key] *= 2
