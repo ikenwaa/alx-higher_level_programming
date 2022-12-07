8e09e7c5a2ea7143f762e37edec61a5f924b92ad
ikenwaa <ajohn9353@gmail.com>
2022-12-07 07:34:23 -0800

    Added 5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    dict_len = list(a_dictionary.keys())
     num = 0
+    dict_len = list(a_dictionary.keys())
 
     for i in dict_len:
         num += i
