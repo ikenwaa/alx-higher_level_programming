92993fe345b9c8aece2d653cc09911775e4e982b
ikenwaa <ajohn9353@gmail.com>
2022-12-07 01:38:49 +0100

    Added linked_lists.c

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -2,11 +2,8 @@
 #define LISTS_H
 
 #include "stdlib.h"
-<<<<<<< HEAD
-=======
 #include "stdio.h"
->>>>>>> 3e8d699fde364baf324185d59a837187aca8d89f
-
+#include "unistd.h"
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -24,6 +21,7 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
+void reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
