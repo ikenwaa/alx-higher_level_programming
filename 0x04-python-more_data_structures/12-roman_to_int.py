108318567b0ac76532ef8b5d24e0d54b61faf747
ikenwaa <ajohn9353@gmail.com>
2022-12-07 15:46:42 -0800

    Added 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) is not str or roman_string == "":
+        return 0
+    num = 0
+    roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+    for i, j in zip(roman_string, roman_string[1:]):
+        if i not in roman_dict.keys():
+            return 0
+        elif roman_dict[i] >= roman_dict[j]:
+            num += roman_dict[i]
+        else:
+            num -= roman_dict[i]
+
+        num += roman_dict[roman_string[-1]]
+        return num
