2f9f28cfa094b903f8d2148cb20a58805bbf2e23
ikenwaa <ajohn9353@gmail.com>
2022-12-07 16:14:31 -0800

    Added 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is not None:
+        total = 0
+        freq = 0
+    
+        for tup in my_list:
+            (weight, tally) = tup
+            total += (weight * tally)
+            freq += tally
+        return (total/freq) if freq > 0 else 0
+    else:
+        return 0
+
