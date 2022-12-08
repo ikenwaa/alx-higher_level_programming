c418e47c804879b242fc195c6e36b65a57419c39
ikenwaa <ajohn9353@gmail.com>
2022-12-07 16:17:24 -0800

    Added 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,7 +3,7 @@ def weight_average(my_list=[]):
     if my_list is not None:
         total = 0
         freq = 0
-    
+
         for tup in my_list:
             (weight, tally) = tup
             total += (weight * tally)
