7ebb84a05a3a2c42550bf3e999cf497becfb9ad8
ikenwaa <ajohn9353@gmail.com>
2022-12-30 06:41:52 -0800

    Added 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -25,7 +25,7 @@ class Square:
         '''Set the value of size'''
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
