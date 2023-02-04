803e3fda76f54ad108d88b8f686d4fa2b6aefa05
ikenwaa <ajohn9353@gmail.com>
2023-02-04 15:41:09 -0800

    Edited *.py files

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""
-Module 4-print_square
-Contains method that prints square with #'s
-Takes in an int size
-"""
+"""Defines a square-printing function."""
 
 
 def print_square(size):
-    """
-    Prints square with #'s given int size
+    """Print a square with the # character.
+    Args:
+        size (int): The height/width of the square.
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    if size == 0:
-        print("", end="")
-    else:
-        print("\n".join(["#" * size for rows in range(size)]))
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
-"""
-Module 5-text_indentation
-Contains method that prints text with 2 new lines after each ".", "?", and ":"
-Takes in a string
-"""
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
-    """
-    Prints text with 2 new lines after each ".", "?", and ":"
+    """Print text with two new lines after each '.', '?', and ':'.
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    for char in ".?:":
-        text = text.replace(char, char + "\n\n")
-    list_lines = [lines.strip(' ') for lines in text.split('\n')]
-    revised = "\n".join(list_lines)
-    print(revised, end="")
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
